b8e897c939af34d0ea4df11a6699da4633403513
Victor <we5705988@gmail.com>
2024-06-08 11:50:02 -0300

    Pequenas mudanças feitas no código-fonte/tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import { CurrentWeather } from "./intefaces";
-import {  apiKey } from '../config.json';
+import { apiKey } from "../config.json";
 
-const getWater = (): Promise<CurrentWeather> => { // Passando a interface utilizada para as propriedades da API;
-    return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${apiKey}`);
-}
+const getWeather = async (): Promise<CurrentWeather> => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${apiKey}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Erro ao retornar informações");
+  }
+
+  const data = await response.json();
+
+  const summary: CurrentWeather = {
+    lat: data.lat,
+    lon: data.lon,
+    current: {
+      temp: data.current.temp, 
+    },
+  };
+
+  return summary;
+};
+
+
+getWeather()
+  .then((weatherSummary) => {
+    console.log(weatherSummary);
+  })
+  .catch((error) => {
+    console.error("Erro:", error);
+  });
